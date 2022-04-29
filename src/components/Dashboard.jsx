17baf870cc83467f8f2d640e77c06bd6ee969bcf
Дмитрий Баранов <dimch2205@gmail.com>
2022-04-29 14:10:53 +0300

    DateRange Picker added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,21 +1,66 @@
 import { useState,useEffect } from "react";
 import styles from "../styles/dashboard.module.scss";
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { DateRange } from 'react-date-range';
+import { addDays } from 'date-fns';
 
 
 export default function Dashboard(props){
 
-	const {children} = props;
+	const {children,data} = props;
+
+	const [pickerShown,showPicker] = useState(false)
+
+	const [dates,setDates] = useState([
+	  {
+	    startDate: new Date(),
+	    endDate: addDays(new Date(), 7),
+	    key: 'selection'
+	  }
+	]);
+
+
+	const processedDate = (type) => {
+		let date = dates[0][type].toString().split(" ")
+		return date[1] + " " + date[2] + "," + date[3]
+
+	}
 
 	return(
 		<section>
 			{children[0]}
-			<div>
 				<div className={`${styles.dashboard} flex flex-column px-[86px] min-h-[250px]`}>
 					<div className={`${styles.dashboard__aside} mt-[32px]`}>
 						{children[1]}
+						{children[2]}
+						{children[3]}
+						{children[4]}
 					</div>
+					<div className={`${styles.dashboard__table} flex flex-col ml-[10%] mt-[12px] w-full`}>
+						<div className={`${styles.table__header} flex justify-between items-center `}>
+							<p className="text-4xl md:text-2xl`">Dashboard</p>
+							<div className={`${styles.header__inputs} inline-flex`}>
+								<label className="inline-flex items-center rounded-sm border-2 border-solid border-gray-300 px-3">
+								<input type="text" value={processedDate("startDate")}/>
+								-
+								<input type="text" value={processedDate("endDate")}/>
+								<span onClick={e=>showPicker(!pickerShown)} style={pickerShown ? {transform:"rotateY(180deg)"} : {transform:"rotateY(0deg)"}}></span>
+								</label>
+							</div>
+						</div>
+					</div>
+				<div className={`${styles.dashboard__datePicker} ${pickerShown ? "" : styles.hidden}`}>
+					<DateRange
+					  onChange={item => setDates([item.selection])}
+					  showSelectionPreview={true}
+					  moveRangeOnFirstSelection={false}
+					  months={2}
+					  ranges={dates}
+					  direction="horizontal"
+					/>
 				</div>
-			</div>		
+				</div>
 		</section>
 	)
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,12 @@ import { useState , useEffect} from "react";
 import Switcher from "../modules/Switcher";
 import Header from "./Header";
 import Dashboard from "./Dashboard";
+import dashboard_styles from "../styles/dashboard.module.scss";
+
 import plus from "../images/plus.png";
+import EvenIcon from "../images/icons/EvenIcon.jsx";
+import OddIcon from "../images/icons/OddIcon.jsx";
+import AllDataIcon from "../images/icons/AllDataIcon.jsx";
 
 
 export default function Layout(props) {
@@ -27,9 +32,11 @@ export default function Layout(props) {
 
 	const [data,setData] = useState([]);
 
-	useEffect(()=>{
+	const [sortType,setSortType] = useState("all");
 
-	},[])
+	useEffect(()=>{
+		sortData()
+	},[sortType])
 
 
 	const chooseCategory = (i) => {
@@ -39,10 +46,32 @@ export default function Layout(props) {
 		setCategories(cat);
 	}
 
+	const sortData = () => {
+		console.log(sortType);
+	}
+
+	const asideSwitches = [
+		{
+			icon:EvenIcon(),
+			text:'Even rows of data',
+			tip:'Display rows 2,4,6 etc'
+		},	
+		{
+			icon: OddIcon(),
+			text:'Odd rows of data',
+			tip:'Display rows 1,3,5 etc'
+		},	
+		{
+			icon:AllDataIcon(),
+			text:'All data',
+			tip:'Display all data'
+		},	
+	]
+
 	return(
-		<main className="container mx-auto bg-white rounded-sm mt-[20px]" style={{overflow:"hidden"}}>
+		<main className="container mx-auto bg-white rounded-sm mt-[20px]" /*style={{overflow:"hidden"}}*/>
 			<Header/>
-		    <Dashboard>
+		    <Dashboard data={data}>
 		    	<Switcher>
 		    		{
 		    			categories.map((category,i) => {
@@ -62,13 +91,28 @@ export default function Layout(props) {
 		    			})
 		    		}
 		    	</Switcher>
-		    		<p className="flex justify-start items-center rounded-lg w-[180px] h-[46px] fz-[20px] bg-white">
+		    		<p className={`${dashboard_styles.aside__create} flex justify-start items-center rounded-lg w-[180px] h-[46px] ml-[7px] mb-[32px] text-xl bg-white`}>
 		    			<img className="w-[16px] h-[16px] mr-[16px] ml-[24px]" src={plus} alt="add"/>
 	    				<span className="justify-self-start" style={{color:"#367BF5",fontWeight:"bold"}}>Create new</span>
 		    		</p>
-		    		<div></div>
-		    		<div></div>
-		    		<div></div>
+		    		{
+		    			asideSwitches.map((switcher,i)=>{
+		    				return (
+		    					<div key={switcher.text.split(" ")[0]} 
+		    						 onClick={(e) => setSortType(switcher.text.split(" ")[0].toLowerCase()) }
+		    						 className={`${dashboard_styles.aside__switcher} ${sortType === switcher.text.split(" ")[0].toLowerCase() ? dashboard_styles.active : '' } 
+		    						 			 flex items-center rounded-sm px-[16px] py-[7px] mb-[8px] min-w-[224px]`
+		    						 }
+		    					>
+		    						{switcher.icon}
+		    						<p className="inline-flex flex-col text-base">
+		    							{switcher.text}
+		    							<span className="text-xs color-gray">{switcher.tip}</span>
+		    						</p>
+		    					</div>
+		    				)
+		    			})
+		    		}
 		    </Dashboard>
 		</main>
 	)
